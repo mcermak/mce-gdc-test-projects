6bd3196c749dc09c9b225b37ccd73ba358a5a69e
Martin Cermak <xcermak.martin@gmail.com>
2014-10-27 17:19:24 +0100

    FEATURE: Dummy csv generator for simple_invoice project

---
+++ projects/single_invoice/csv_generator/gen_invoice_csv.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use utf8;
+
+use DateTime;
+use constant GEN_ROWS => 10_000_000;
+#===============================================================================
+my $names;
+foreach my $fname (qw/firstnames surnames/) {
+	open(my $f_names,"<","$fname.txt");
+	my @names = <$f_names>;
+	$names->{$fname} = [ map { chomp $_; $_ } @names ];
+	close($f_names);
+}
+
+sub date_gen {
+	my $scale = shift;
+
+	my $now = DateTime->now;
+
+	my $start_of_last_year = DateTime->new( year => $now->year);
+	$now->subtract( days => int( rand($scale) ) );
+	return '"'.$now->ymd('-').'"';
+}
+
+sub gen_name {
+	my $names = shift;
+
+	my @surnames = @{$names->{surnames}};
+	my @names = @{$names->{firstnames}};
+
+	return (
+		$names[int(rand(scalar @names))],
+		$surnames[int(rand(scalar @surnames))]
+	);
+}
+
+
+my $size = $ARGV[1];
+my $start = $ARGV[0];
+print join(",", qw/
+	tab_inv_item.col_quantity
+	tab_inv_item.col_total
+	tab_inv.col_total
+	tab_inv.col_name
+	tab_inv.dt_invoice
+	tab_pers.col_bn
+	tab_pers.col_fn
+	tab_pers.col_sn/),$/;
+foreach ($start..$size) {
+	print join(",", int(rand(30)), $_, int(rand(100)), "inv_".(int(rand(($size-$start)*2)+$start)), date_gen(90), $_, gen_name($names)), $/;
+}
+
